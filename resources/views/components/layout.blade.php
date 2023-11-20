4d43dd1cda565d041d9f5236f6ece359ce99d31e
joaoalvesss <up202108670@up.pt>
2023-11-20 22:10:48 +0000

    Started making cart

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -45,6 +45,18 @@
                     </form>
                     <ul class="navbar-nav mb-2 mb-lg-0">
                         @auth
+                        <li>
+                          <a class="nav-link" href="/wishlist">
+                            <i class="fa-solid fa-heart"></i>
+                            Wishlist
+                        </a>                          
+                        </li>
+                        <li>
+                          <a class="nav-link" href="/cart">
+                            <i class="fa-solid fa-car"></i>
+                            Cart
+                        </a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/profile">
                                 <i class="fa-solid fa-user"></i>
