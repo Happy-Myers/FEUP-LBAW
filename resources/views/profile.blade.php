82f6ed90ba9391d83c67430d8f5deeaa5f151dca
joaoalvesss <up202108670@up.pt>
2023-11-22 19:02:13 +0000

    Applied css to profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,36 +3,39 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <div class="card">
-                    <div class="card-header" style="background-color: #00101C; color: white;">
+                    <h2 class="card-header">
                         Profile
-                    </div>
-
-                    <div class="card-body" style="background-color: #001C30; color: white;">
+                    </h2>
+                    <div class="card-body">
                         <div class="row">
                             <div class="col-md-4">
                                 <!-- Profile Picture -->
-                                <img src="{{ asset('path/to/profile-pic.jpg') }}" alt="Profile Picture" class="img-fluid rounded-circle">
+                                <!-- <img src="{{ asset('path/to/profile-pic.jpg') }}" alt="Profile Picture" class="img-fluid rounded-circle"> -->
+                                <img src="{{ asset('images/pikachu.jpg') }}" alt="Profile Picture" class="img-fluid w-200 h-200 rounded">
+
                             </div>
-                            <div class="col-md-8">
+                            <div class="col details">
                                 <!-- User Details -->
                                 <h3 class="mb-2">{{ $user->name }}</h3>
-                                <p class="mb-0" style="font-weight: bold;">Username:</p>
+                                <p class="mb-0">Username:</p>
                                 <p class="mb-2">{{ $user->name }}</p>
-                                <p class="mb-0" style="font-weight: bold;">E-mail:</p>
+                                <p class="mb-0">E-mail:</p>
                                 <p class="mb-4">{{ $user->email }}</p>
-                                <div class="d-inline-block mr-2">
+                                <div class="align-buttons">
+                                <div class="buttons">
                                     <a href="#" class="btn btn-info">
                                         <div class="d-flex align-items-center">
                                             Edit Profile
                                         </div>
                                     </a>
                                 </div>
-                                <div class="d-inline-block">
+                                <div class="buttons">
                                     <a href="#" class="btn btn-danger">
                                         <div class="d-flex align-items-center">
                                             Delete Account
                                         </div>
                                     </a>
+                                </div>
                                 </div>
                            </div>
                         </div>
@@ -41,11 +44,11 @@
                  </div>
 
                 <div class="card mt-3 mb-5" >
-                    <div class="card-header" style="background-color: #00101C; color: white;">
+                    <h2 class="card-header">
                         Saved Addresses
-                    </div>
+                    </h2>
 
-                    <div class="card-body" style="background-color: #001C30; color: white;">
+                    <div class="card-body">
                         <div class="address-box">
                             <h4>{{ "title" }}</h4>
                             <p>{{ "address" }}</p>
