5c2b1305df05bf1c7d7e582c60102b03172b865d
Gonçalo Marques <up202006874@edu.fe.up.pt>
2023-12-08 23:52:08 +0000

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <div class="container text-white">
+        <h1>Sorry, this page isn't available</h1>
+        <p>The link you followed may be broken, or the page may have been removed.<a href="{{url('/')}}" class="text-decoration-none">Go back to home page</a></p>
+    </div>
+</x-layout>
